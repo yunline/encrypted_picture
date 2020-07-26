e7e5e197fdf9a7b53353e0aa2ecb24329a505a8f
yunline <yunline@foxmail.com>
2020-07-26 20:36:05 +0800

    写好啦~

---
+++ decode.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import gzip
+import time
+
+fn=input("输入文件名>>>")
+print("解码中。。。")
+t0=time.time()
+
+pic=cv2.imread(fn,-1)
+
+def main():
+    content_=b""
+    for n in np.nditer(pic):
+        content_+=n.tobytes()
+    return content_
+
+content=main()
+print(time.time()-t0)
+
+szzz=48
+fnlll=48
+length=int.from_bytes(content[:szzz+fnlll],"big")
+final=content[szzz+fnlll:szzz+fnlll+length]
+fn="_"+content[szzz:szzz+fnlll].decode().replace("\x00","")
+
+with open(fn,"wb") as ffff:
+    ffff.write(gzip.decompress(final))
---
+++ encode.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import os
+import math
+import gzip
+
+fn=input("输入文件名>>>")
+print("加密中。。。")
+szzz=48
+fnlll=48
+fnnn=os.path.basename(fn).encode("utf8")
+if len(fnnn)<fnlll:
+    fnnn+=b"\x00"*(fnlll-len(fnnn))
+else:
+    raise Exception("文件名太长啦")
+
+with open(fn,"rb") as f:
+    fffff=gzip.compress(f.read(),9)
+    fsize=len(fffff)
+    _fs=fsize+szzz+fnlll
+    size=int(math.sqrt(_fs//4+(1 if _fs%4 else 0)))+1
+    pic=np.zeros((size,size,4),dtype=np.uint8)
+    content=fsize.to_bytes(szzz,"big")+fnnn+fffff
+    ln=0
+    col=0
+    rgba=-1
+    for b in content:
+        if rgba<3:
+            rgba+=1
+        else:
+            rgba=0
+            if col<size-1:
+                col+=1
+            else:
+                col=0
+                if ln<size-1:
+                    ln+=1
+                else:
+                    raise Exception("出大问题了QAQ")
+        pic[ln][col][rgba]=b
+    cv2.imwrite("result.png",pic)
